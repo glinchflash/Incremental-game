e1da5a9585135a16acfcdd0cde4a63728d725871
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-05-16 15:55:57 +0200

    added timed boost and upgrades refined

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Incremental-game
+name: LoL-minion clicker
+
+---
+# LoL-minion clicker
+<p>purpose: make an incremental game based on the original cookie clicker
+</p>
+
+___
+
--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ let totalAD = 0;
 let clickingPower = 1;
 let bfCost = 120;
 let totalbf = 0;
+//variables for temporary boost
+let ieCost = 1000;
+let timeLeft = 30;
 
 
 //function to add 1 to gold/score per click
@@ -24,19 +27,8 @@ function buyAD() {
         gold = gold - adCost;
         totalAD = totalAD + 1;
         adCost = Math.round(adCost * 1.2);
-        clickingPower = clickingPower + 0.4;
-        if (totalAD >= 10) {
-            clickingPower = clickingPower + 0.6;
-        }
-        if (totalAD >= 25) {
-            clickingPower = clickingPower + 0.9;
-        }
-        if (totalAD >= 50) {
-            clickingPower = clickingPower + 1.2;
-        }
-        if (totalAD >= 100) {
-            clickingPower = clickingPower + 2.5;
-        }
+        clickingPower = clickingPower + 1;
+
         document.getElementById("gold").innerHTML = gold.toFixed(2);
         document.getElementById("adCost").innerHTML = adCost;
         document.getElementById("totalAD").innerHTML = totalAD;
@@ -50,7 +42,7 @@ function buyBF() {
     if (gold >= bfCost) {
         gold = gold - bfCost;
         totalbf = totalbf + 1;
-        bfCost = Math.round(bfCost * 1.6);
+        bfCost = Math.round(bfCost * 1.15);
         document.getElementById("gold").innerHTML = gold.toFixed(2);
         document.getElementById("bfCost").innerHTML = bfCost;
         document.getElementById("totalbf").innerHTML = totalbf;
@@ -59,16 +51,67 @@ function buyBF() {
 
 //set interval to make score/gold go higher automatic based on upgrade 2 // + 1000ms = 1second
 setInterval(function () {
-    if (totalbf >= 15) {
-        gold = gold + Math.floor(totalbf * 1.25);
-    } else {
-        gold = gold + totalbf;
+    gold = gold + Math.floor(totalbf);
+    if (totalbf >= 10) {
+        gold = gold + Math.floor(totalbf * 1.5);
+    }
+    if (totalbf >= 25) {
+        gold = gold + Math.floor(totalbf * 2);
+    }
+    if (totalbf >= 50) {
+        gold = gold + Math.floor(totalbf * 3);
+    }
+    if (totalbf >= 100) {
+        gold = gold + Math.floor(totalbf * 8);
     }
     document.getElementById("gold").innerHTML = gold.toFixed(2);
 
 }, 1000);
 
 // update title gold
-setInterval(function (){
+setInterval(function () {
     document.title = gold.toFixed(2) + " gold - Lol-minion Clicker";
-},1);
+}, 1);
+
+
+//200% income for 30 seconds
+/*
+document.getElementById("infinityEdge").addEventListener("click", setInterval(function () {
+    if (gold >= ieCost) {
+        clickingPower = clickingPower * 2;
+        if (timeleft <= 0) {
+            alert("Multiplier has ended!")
+        }
+        timeLeft -= 1;
+        document.getElementById('infinityEdge').innerHTML = timeLeft;
+
+    }
+
+},1000) );*/
+
+document.getElementById('infinityEdge').addEventListener("click", multiply);
+
+function multiply() {
+    if (gold >= ieCost) {
+        gold = gold - ieCost;
+        ieCost = Math.round(ieCost * 1.85);
+        clickingPower = clickingPower * 2;
+
+        let multiplier = setInterval(function () {
+
+            timeLeft -= 1;
+            document.getElementById('infinityEdge').innerHTML = "Time left: " + timeLeft;
+            if (timeLeft <= 0) {
+                clickingPower = clickingPower / 2;
+                clearInterval(multiplier);
+                document.getElementById('infinityEdge').innerHTML = "Infinity Edge " + ieCost;
+            }
+        }, 1000)
+
+    }
+}
+
+
+
+
+
